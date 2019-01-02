c23f23e1507a559e056bdd39fdac49caab37070d
Fernando Ramos <ferramgon1@gmail.com>
2019-01-02 18:03:55 +0100

    applies lint

--- src/base/routes/index.jsx
+++ src/base/routes/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
-import Main from 'containers/Main/';
+import Main from 'containers/Main';
 import App from 'containers/App';
 
 const Routes = () => (

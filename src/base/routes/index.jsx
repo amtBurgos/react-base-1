63d348df58f78d92da10c9c81f2a9ec419ec1cf0
Miguel Teran <650687@CTSCEM0045.local>
2019-01-02 15:27:08 +0100

    Removed files

--- src/base/routes/index.jsx
+++ src/base/routes/index.jsx
@@ -1,16 +1,12 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
-import Calculator from 'containers/Calculator/';
 import Main from 'containers/Main/';
-import WeatherStations from 'containers/WeatherStations/';
 import App from 'containers/App';
 
 const Routes = () => (
   <Route path="/" component={ () => (
     <App>
       <Route exact={ true } path="/" component={ Main } />
-      <Route path="/calculator" component={ Calculator } />
-      <Route path="/weatherstations" component={ WeatherStations } />
     </App>
   ) } />
 );

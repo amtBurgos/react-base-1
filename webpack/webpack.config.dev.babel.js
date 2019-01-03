05dfb545360d405c7537f27cc2be66f1f431990c
Fernando Ramos <ferramgon1@gmail.com>
2019-01-03 10:25:28 +0100

    adds CSS HMR

--- webpack/webpack.config.dev.babel.js
+++ webpack/webpack.config.dev.babel.js
@@ -12,7 +12,7 @@ export const { context } = common;
 export const { resolve } = common;
 
 export const entry = {
-  app: common.clientPath,
+  app: common.clientPath
 };
 
 export const devServer = {
@@ -20,7 +20,7 @@ export const devServer = {
   compress: true,
   port: 9000,
   historyApiFallback: true,
-  hot: true,
+  hot: true
 };
 
 export const output = {
@@ -29,7 +29,7 @@ export const output = {
   library: '[name]',
   filename: '[name].js',
   sourceMapFilename: '[name].map',
-  chunkFilename: '[name].chunk.js',
+  chunkFilename: '[name].chunk.js'
 };
 
 export const module = {
@@ -38,32 +38,33 @@ export const module = {
       test: /\.(js|jsx)$/,
       include: /src/,
       exclude: [/node_modules/, /dist/, /server/],
-      use: ['babel-loader'],
+      use: ['babel-loader']
     },
     {
       test: /\.css$/,
       exclude: /node_modules/,
       use: [
+        'css-hot-loader',
         {
-          loader: 'style-loader',
+          loader: 'style-loader'
         },
         {
           loader: 'css-loader',
           options: {
             modules: true,
             importLoaders: 1,
-            localIdentName: '[local]-[hash:base64:4]',
-          },
+            localIdentName: '[local]-[hash:base64:4]'
+          }
         },
         {
           loader: 'postcss-loader',
           options: {
-            plugins: () => common.postcss,
-          },
-        },
-      ],
-    },
-  ]),
+            plugins: () => common.postcss
+          }
+        }
+      ]
+    }
+  ])
 };
 
 export const plugins = [
@@ -71,15 +72,15 @@ export const plugins = [
   new webpack.HotModuleReplacementPlugin(),
   new webpack.DllReferencePlugin({
     context: common.buildPath,
-    manifest: require('../dist/dlls/vendor-manifest.json'),
+    manifest: require('../dist/dlls/vendor-manifest.json')
   }),
   new CopyWebpackPlugin([
-    { from: path.resolve(__dirname, '../src/app/assets'), to: common.assetsPath },
+    { from: path.resolve(__dirname, '../src/app/assets'), to: common.assetsPath }
   ]),
   new HtmlWebpackPlugin({
     title: 'React base',
     inject: 'body',
     filename: 'index.html',
-    template: path.resolve(common.clientPath, 'index.html'),
-  }),
+    template: path.resolve(common.clientPath, 'index.html')
+  })
 ].concat(common.plugins);
